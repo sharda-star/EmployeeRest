aa22f1b25b5b2c854d5996f8c073d0a6dc088e2f
sawtadkar_s <sawtadkar_s@Sharda-SW.India.XoriantCorp.com>
2020-03-19 12:28:24 +0530

    1.Stage first Change

---
+++ src/main/java/com/sharda/guid/SpringBootRestEmployeeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sharda.guid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestEmployeeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestEmployeeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharda/guid/SpringBootRestEmployeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharda.guid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestEmployeeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
